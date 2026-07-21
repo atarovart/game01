ba94579a185dd91325784e0ec513058c6f4e2be5
atarovart <at.art7100@gmail.com>
2026-07-21 11:15:38 +0700

    day3

--- character/character_body_2d.gd
+++ character/character_body_2d.gd
@@ -1,15 +1,20 @@
 extends CharacterBody2D
-
+const SEED = preload("res://weapons/seed.tscn")
 
 const SPEED = 150.0
-
-func _physics_process(delta):
-	# Получаем направление движения: -1 вверх, 1 вниз
-	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
+func shot(): 
+	var seedd = SEED.instantiate()
+	add_child(seedd)
 	
-	# Устанавливаем скорость
+func _physics_process(delta: float):
+	if Input.is_action_just_pressed("ui_accept"):
+		shot()
+		
+	var input_vector = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
 	velocity = input_vector * SPEED
 
-	# Двигаем персонажа
 	move_and_slide()
+	
+
+		
 	
---
+++ weapons/seed.gd
@@ -0,0 +1,6 @@
+extends CharacterBody2D
+
+const SPEED = 100.0
+
+func _physics_process(delta):
+	move_and_collide(Vector2.RIGHT * SPEED * delta)
